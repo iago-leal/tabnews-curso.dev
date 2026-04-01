9f1f6278dca160958a1b418353192cebf29c1475
Iago Leal <iagolealdev@gmail.com>
2026-04-01 13:08:31 +0000

    conserta frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu amo a Ana Luisa Ferreira Marques. Ela é o amor da minha vida!\n Se você viu, me manda um 💕 no whatsapp.</h1>
+    return <h1>Eu amo a Ana Luisa Ferreira Marques. Ela é o amor da minha vida! Se você viu, me manda um 💕 no whatsapp.</h1>
 }
 
 export default Home;
